987dd4af1952f2f89a571c5568bc287e83b75ca9
KyuCheol Hwang <97iambigman@gmail.com>
2023-04-20 17:01:19 +0900

    feat(/utils/Input) : 사용자 입력을 처리해주는 기능 추가

---
+++ src/main/java/racingcar/utils/Input.java
@@ -0,0 +1,33 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class Input {
+
+    public static List<String> getCarNames() {
+        while (true) {
+            String input = Console.readLine();
+            try {
+                Validator.validateCarNames(input);
+            } catch (IllegalArgumentException e) {
+                Output.displayError(e);
+                continue;
+            }
+            return Resolver.resolveCarNames(input);
+        }
+    }
+
+    public static int getCount() {
+        while (true) {
+            String input = Console.readLine();
+            try {
+                Validator.validateCount(input);
+            } catch (IllegalArgumentException e) {
+                Output.displayError(e);
+            }
+            return Resolver.resolveCount(input);
+        }
+    }
+}
